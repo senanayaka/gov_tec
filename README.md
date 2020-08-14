546fa0204bc3f7fee5f8a947bb3ef822dfae8a6b
para <parakrama.maralanda@stengg.com>
2020-08-14 13:17:13 +0800

    Changes for Add swagger to application

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 #Govtec 
+
+## Running the app
+
+```bash
+# development
+$ npm start
+
+# watch mode
+$ npm start:dev
+
+# production mode
+$ npm start:prod
+
+# analyze production mode
+$ npm start:analyze
+
+# using with Docker
+$ ./scripts/up.sh # to start
+$ ./scripts/down.sh # to stop
+```
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,35 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { ValidationPipe, Logger } from '@nestjs/common';
+import fastifyCors from 'fastify-cors';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  const documentOptions = new DocumentBuilder()
+    .setTitle('govtec')
+    .setDescription('Users API Microservice')
+    .setVersion('1.0')
+    .setBasePath('/api')
+    .build();
+  const document = SwaggerModule.createDocument(app, documentOptions);
+  const validationOptions = {
+    skipMissingProperties: true,
+    validationError: { target: false },
+  };
+
+  app.useGlobalPipes(new ValidationPipe(validationOptions));
+  app.setGlobalPrefix('v1');
+  
+  // app.register(fastifyCors, {
+  //   origin: true,
+  // });
+
+  SwaggerModule.setup('/api', app, document);
+
   await app.listen(3000);
+
 }
 bootstrap();
